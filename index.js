017e7768813a9a108871d6e7556ef7c6934687f7
Ondicho <albert.ondicho@moringaschool.com>
2022-06-19 08:07:09 +0300

    commit tree 0b2e545402906b4a04a8556ebef7aefbd254dbd6

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+const response = document.querySelector("#response");
+const message = document.querySelector("#error");
+const user = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+const userObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(user),
+};
+
+function submitData() {
+  fetch("http://localhost:3000/users", userObject)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(response);
+      response.innerHTML = `${data.id}`;
+    })
+    .catch((error) => {
+      message.innerHTML = `${error}`;
+
+      console.log(error);
+    });
+}
+
+// submitData()
+
+//
